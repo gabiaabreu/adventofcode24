c09b342102c776d2f9ccfb328f372cf07be6bc93
gabiaabreu <gabiabr3u@gmail.com>
2024-12-02 12:49:51 -0300

    feat: enhancements Day 2 - Red-Nosed Reports

--- src/Day2.java
+++ src/Day2.java
@@ -13,92 +13,74 @@ public class Day2 {
         File file = new File("./src/resources/day2_input.txt");
         BufferedReader reader = new BufferedReader(new FileReader(file));
 
-        List<int[]> reportsList = new ArrayList<>();
+        List<List<Integer>> reportsList = new ArrayList<>();
 
         String line;
         while ((line = reader.readLine()) != null) {
-            var intArray = Arrays.stream(line.trim().split("\\s+")).mapToInt(Integer::parseInt).toArray();
-            reportsList.add(intArray);
+            var intList = Arrays.stream(line.trim().split("\\s+"))
+                    .map(Integer::valueOf)
+                    .toList();
+
+            reportsList.add(intList);
         }
 
-//        for (int[] array : reportsList) {
-//            System.out.println(Arrays.toString(array));
-//        }
-
-        // part one
-        // check safe
+        // part one - check safe
         var safeCount = 0;
-        List<int[]> unsafeReportsList = new ArrayList<>();
+        var conditionalSafeCount = 0;
 
         for (var report : reportsList) {
-            var reportList = Arrays.stream(report).boxed().toList(); // TODO: ja salvar como List la em cima
-            if (isIncreasing(reportList) || isDecreasing(reportList)) {
+            if (isSortedSafely(report)) {
                 safeCount++;
             } else {
-                unsafeReportsList.add(report);
+                // part two - check conditional safe
+                if (isConditionalSafe(report)) {
+                    conditionalSafeCount++;
+                }
             }
         }
-
-        System.out.println("Safe count: " + safeCount);
-
-        // part two
-        // check conditional safe
-        var conditionalSafeCount = 0;
-        for (var report : unsafeReportsList) {
-            if (isConditionalSafe(report)) {
-                conditionalSafeCount++;
-            }
-        }
-
-        System.out.println("Conditional safe count: " + conditionalSafeCount);
 
         var total = safeCount + conditionalSafeCount;
+        System.out.println("Safe count: " + safeCount);
+        System.out.println("Conditional safe count: " + conditionalSafeCount);
         System.out.println("Total safe count: " + total);
-
     }
 
-    private static boolean isConditionalSafe(int[] report) {
-        List<Integer> list = new ArrayList<>(Arrays.stream(report).boxed().toList());
-
-        for (int i = 0; i < list.size(); i++) {
-            // remove number in given index, create newList without it
+    private static boolean isConditionalSafe(List<Integer> report) {
+        for (int i = 0; i < report.size(); i++) {
+            // removes value in given index, creates newList without it
             // if newList passes on isIncreasing/isDecreasing
             // then it's safe
-
-            List<Integer> newList = new ArrayList<>(list);
+            List<Integer> newList = new ArrayList<>(report);
             newList.remove(i);
 
-            if (isIncreasing(newList) || isDecreasing(newList)) {
+            if (isSortedSafely(newList)) {
                 return true;
             }
         }
-
         return false;
     }
 
-    private static boolean isIncreasing(List<Integer> report) {
+    private static boolean isSortedSafely(List<Integer> report) {
+        var increasing = false;
+        var decreasing = false;
+
         for (int i = 0; i < report.size() - 1; i++) {
             var current = report.get(i);
             var next = report.get(i + 1);
 
-            if (current >= next || (next - current > 3)) {
+            // absolute difference can't be greater than 3
+            // numbers can't be equal
+            if (current.equals(next) || Math.abs(next - current) > 3) {
                 return false;
             }
-        }
 
-        return true;
-    }
-
-    private static boolean isDecreasing(List<Integer> report) {
-        for (int i = 0; i < report.size() - 1; i++) {
-            var current = report.get(i);
-            var next = report.get(i + 1);
-
-            if (current <= next || (current - next > 3)) {
-                return false;
+            if (current > next) {
+                decreasing = true;
+            }
+            if (next > current) {
+                increasing = true;
             }
         }
-
-        return true;
+        return increasing ^ decreasing;
     }
 }
