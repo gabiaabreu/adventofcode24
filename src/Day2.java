5aecd3556f3fd8d805e4ece2df35f4bb797ef687
gabiaabreu <gabiabr3u@gmail.com>
2024-12-02 12:23:26 -0300

    feat: Day 2 - Red-Nosed Reports

--- src/Day2.java
+++ src/Day2.java
@@ -20,25 +20,66 @@ public class Day2 {
             var intArray = Arrays.stream(line.trim().split("\\s+")).mapToInt(Integer::parseInt).toArray();
             reportsList.add(intArray);
         }
+
 //        for (int[] array : reportsList) {
 //            System.out.println(Arrays.toString(array));
 //        }
 
-        // check increasing
+        // part one
+        // check safe
         var safeCount = 0;
+        List<int[]> unsafeReportsList = new ArrayList<>();
+
         for (var report : reportsList) {
-            if (isIncreasing(report) || isDecreasing(report)) {
+            var reportList = Arrays.stream(report).boxed().toList(); // TODO: ja salvar como List la em cima
+            if (isIncreasing(reportList) || isDecreasing(reportList)) {
                 safeCount++;
+            } else {
+                unsafeReportsList.add(report);
             }
         }
 
-        System.out.println(safeCount);
+        System.out.println("Safe count: " + safeCount);
+
+        // part two
+        // check conditional safe
+        var conditionalSafeCount = 0;
+        for (var report : unsafeReportsList) {
+            if (isConditionalSafe(report)) {
+                conditionalSafeCount++;
+            }
+        }
+
+        System.out.println("Conditional safe count: " + conditionalSafeCount);
+
+        var total = safeCount + conditionalSafeCount;
+        System.out.println("Total safe count: " + total);
+
     }
 
-    private static boolean isIncreasing(int[] report) {
-        for (int i = 0; i < report.length - 1; i++) {
-            var current = report[i];
-            var next = report[i + 1];
+    private static boolean isConditionalSafe(int[] report) {
+        List<Integer> list = new ArrayList<>(Arrays.stream(report).boxed().toList());
+
+        for (int i = 0; i < list.size(); i++) {
+            // remove number in given index, create newList without it
+            // if newList passes on isIncreasing/isDecreasing
+            // then it's safe
+
+            List<Integer> newList = new ArrayList<>(list);
+            newList.remove(i);
+
+            if (isIncreasing(newList) || isDecreasing(newList)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean isIncreasing(List<Integer> report) {
+        for (int i = 0; i < report.size() - 1; i++) {
+            var current = report.get(i);
+            var next = report.get(i + 1);
 
             if (current >= next || (next - current > 3)) {
                 return false;
@@ -48,10 +89,10 @@ public class Day2 {
         return true;
     }
 
-    private static boolean isDecreasing(int[] report) {
-        for (int i = 0; i < report.length - 1; i++) {
-            var current = report[i];
-            var next = report[i + 1];
+    private static boolean isDecreasing(List<Integer> report) {
+        for (int i = 0; i < report.size() - 1; i++) {
+            var current = report.get(i);
+            var next = report.get(i + 1);
 
             if (current <= next || (current - next > 3)) {
                 return false;
