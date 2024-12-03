d2589750e390e67f965ccbc8d94128d534ae47cd
gabiaabreu <gabiabr3u@gmail.com>
2024-12-03 10:09:18 -0300

    feat: Day 3 - Mull It Over

--- src/Day3.java
+++ src/Day3.java
@@ -7,33 +7,54 @@ import java.util.regex.Pattern;
 
 public class Day3 {
 
-        public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-            // read entire file
-            String input = Files.readString(Path.of("./src/resources/day3_input.txt"));
+        // read entire file
+        String input = Files.readString(Path.of("./src/resources/day3_input.txt"));
+        input = input.replaceAll("\\r?\\n", ""); // remove line breaks -> game changer
 
-            var regex = "mul\\((\\d{1,3}),(\\d{1,3})\\)";
-            Pattern pattern = Pattern.compile(regex);
-            Matcher matcher = pattern.matcher(input);
+        // part two
+        var regexDoDont = "do\\(\\)(.*?)don't\\(\\)";
+        List<String> doSections = new ArrayList<>();
 
-            var result = 0;
-            List<String> matchesFound = new ArrayList<>();
+        var parts = input.split("don't\\(\\)", 2);
+        var firstPartTilDont = parts[0];
+        var rest = parts[1];
 
-            while (matcher.find()) {
-                String match = matcher.group(); // mul(x,y)
-                matchesFound.add(match);
+        Pattern pattern = Pattern.compile(regexDoDont);
+        Matcher matcher = pattern.matcher(rest);
 
-                var x = Integer.parseInt(matcher.group(1));
-                var y = Integer.parseInt(matcher.group(2));
+        while (matcher.find()) {
+            String match = matcher.group();
 
-                var multiplication = x * y;
-                result = result + multiplication;
-            }
-
-            for (var found : matchesFound) {
-                System.out.println("Found " + found);
-            }
-
-            System.out.println(result);
+            doSections.add(match);
         }
+
+        var textToConsider = firstPartTilDont + doSections;
+
+        // part one
+        var mulRegex = "mul\\((\\d{1,3}),(\\d{1,3})\\)";
+        Pattern mulPattern = Pattern.compile(mulRegex);
+        Matcher mulMatcher = mulPattern.matcher(textToConsider);
+
+        var result = 0;
+        List<String> matchesFound = new ArrayList<>();
+
+        while (mulMatcher.find()) {
+            String match = mulMatcher.group(); // mul(x,y)
+            matchesFound.add(match);
+
+            var x = Integer.parseInt(mulMatcher.group(1));
+            var y = Integer.parseInt(mulMatcher.group(2));
+
+            var multiplication = x * y;
+            result = result + multiplication;
+        }
+
+        for (var found : matchesFound) {
+            System.out.println("Found " + found);
+        }
+
+        System.out.println(result);
+    }
 }
