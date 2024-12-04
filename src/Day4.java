1d17283cb337d6db956993da30fa608ecda28fd5
gabiaabreu <gabiabr3u@gmail.com>
2024-12-04 20:04:24 -0300

    feat: adds matrix rotation

--- src/Day4.java
+++ src/Day4.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Day4 {
@@ -56,34 +55,45 @@ public class Day4 {
                             && matrix[i + 1][j - 1] == 'S' && matrix[i + 1][j + 1] == 'M') {
                         crossedMasFound++;
                     }
+                }
+            }
+        }
 
-                    //  M   M
-                    //    A
-                    // S    S
-                    if (matrix[i - 1][j - 1] == 'M' && matrix[i - 1][j + 1] == 'M'
-                            && matrix[i + 1][j - 1] == 'S' && matrix[i + 1][j + 1] == 'S') {
-                        crossedMasFound++;
-                    }
+        // if i rotate matrix 90 degrees for 3 times and look again, i'll be able to find:
+        //  M   M         S   S           M   S
+        //    A             A               A
+        // S    S        M    M           M   S
+        var matrixCopy = matrix;
+        for (int k = 0; k < 3; k++) {
+            matrixCopy = rotateMatrix90Degrees(matrixCopy);
 
-                    //  S   S
-                    //    A
-                    // M    M
-                    if (matrix[i - 1][j - 1] == 'S' && matrix[i - 1][j + 1] == 'S'
-                            && matrix[i + 1][j - 1] == 'M' && matrix[i + 1][j + 1] == 'M') {
-                        crossedMasFound++;
-                    }
-
-                    //  M   S
-                    //    A
-                    // M    S
-                    if (matrix[i - 1][j - 1] == 'M' && matrix[i - 1][j + 1] == 'S'
-                            && matrix[i + 1][j - 1] == 'M' && matrix[i + 1][j + 1] == 'S') {
-                        crossedMasFound++;
+            for (int i = 1; i < matrixCopy.length - 1; i++) {
+                for (int j = 1; j < matrixCopy[i].length - 1; j++) {
+                    if (matrixCopy[i][j] == 'A') {
+                        if (matrixCopy[i - 1][j - 1] == 'S' && matrixCopy[i - 1][j + 1] == 'M'
+                                && matrixCopy[i + 1][j - 1] == 'S' && matrixCopy[i + 1][j + 1] == 'M') {
+                            crossedMasFound++;
+                        }
                     }
                 }
             }
         }
         System.out.println("number of crossed MAS found: " + crossedMasFound);
+    }
+
+    public static char[][] rotateMatrix90Degrees(char[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        char[][] rotated = new char[n][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                rotated[j][m - 1 - i] = matrix[i][j];
+            }
+        }
+
+        return rotated;
     }
 
     private static void findXmas(char[][] matrix) {
@@ -97,30 +107,6 @@ public class Day4 {
                         xmasFound++;
                     }
 
-                    // SAMX
-                    if (j - 3 >= 0
-                            && matrix[i][j - 1] == 'M' && matrix[i][j - 2] == 'A' && matrix[i][j - 3] == 'S') {
-                        xmasFound++;
-                    }
-
-                    // X
-                    // M
-                    // A
-                    // S
-                    if (i + 3 < matrix.length
-                            && matrix[i + 1][j] == 'M' && matrix[i + 2][j] == 'A' && matrix[i + 3][j] == 'S') {
-                        xmasFound++;
-                    }
-
-                    // S
-                    // A
-                    // M
-                    // X
-                    if (i - 3 >= 0
-                            && matrix[i - 1][j] == 'M' && matrix[i - 2][j] == 'A' && matrix[i - 3][j] == 'S') {
-                        xmasFound++;
-                    }
-
                     // X
                     //   M
                     //     A
@@ -129,32 +115,48 @@ public class Day4 {
                             && matrix[i + 1][j + 1] == 'M' && matrix[i + 2][j + 2] == 'A' && matrix[i + 3][j + 3] == 'S') {
                         xmasFound++;
                     }
+                }
+            }
+        }
 
-                    // S
-                    //   A
-                    //     M
-                    //       X
-                    if (i - 3 >= 0 && j - 3 >= 0
-                            && matrix[i - 1][j - 1] == 'M' && matrix[i - 2][j - 2] == 'A' && matrix[i - 3][j - 3] == 'S') {
-                        xmasFound++;
+        // if i rotate matrix 90 degrees for 3 times and look again, i'll be able to find:
+        // S A M X
+        // X             S
+        // M             A
+        // A             M
+        // S             X
+        var matrixCopy = matrix;
+        for (int k = 0; k < 3; k++) {
+            matrixCopy = rotateMatrix90Degrees(matrixCopy);
+
+            for (int i = 0; i < matrixCopy.length; i++) {
+                for (int j = 0; j < matrixCopy[i].length; j++) {
+                    if (matrixCopy[i][j] == 'X') {
+                        if (matrixCopy[i].length > j + 3
+                                && matrixCopy[i][j + 1] == 'M' && matrixCopy[i][j + 2] == 'A' && matrixCopy[i][j + 3] == 'S') {
+                            xmasFound++;
+                        }
                     }
+                }
+            }
+        }
 
-                    //       X
-                    //     M
-                    //   A
-                    // S
-                    if (i + 3 < matrix.length && j - 3 >= 0
-                            && matrix[i + 1][j - 1] == 'M' && matrix[i + 2][j - 2] == 'A' && matrix[i + 3][j - 3] == 'S') {
-                        xmasFound++;
-                    }
+        // if i rotate matrix 90 degrees for 3 times and look again, i'll be able to find:
+        // S                       X             S
+        //   A                   M             A
+        //     M               A             M
+        //       X           S             X
+        var diagonalMatrixCopy = matrix;
+        for (int k = 0; k < 3; k++) {
+            diagonalMatrixCopy = rotateMatrix90Degrees(diagonalMatrixCopy);
 
-                    //       S
-                    //     A
-                    //   M
-                    // X
-                    if (i - 3 >= 0 && j + 3 < matrix[i].length
-                            && matrix[i - 1][j + 1] == 'M' && matrix[i - 2][j + 2] == 'A' && matrix[i - 3][j + 3] == 'S') {
-                        xmasFound++;
+            for (int i = 0; i < diagonalMatrixCopy.length; i++) {
+                for (int j = 0; j < diagonalMatrixCopy[i].length; j++) {
+                    if (diagonalMatrixCopy[i][j] == 'X') {
+                        if (i + 3 < diagonalMatrixCopy.length && j + 3 < diagonalMatrixCopy[i].length
+                                && diagonalMatrixCopy[i + 1][j + 1] == 'M' && diagonalMatrixCopy[i + 2][j + 2] == 'A' && diagonalMatrixCopy[i + 3][j + 3] == 'S') {
+                            xmasFound++;
+                        }
                     }
                 }
             }
