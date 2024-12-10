7e506b656b969f7038d971ecfd51bee92c4e5598
gabiaabreu <gabiabr3u@gmail.com>
2024-12-10 11:57:11 -0300

    feat: Day 9 - Disk Fragmenter

--- src/Day9.java
+++ src/Day9.java
@@ -10,9 +10,10 @@ public class Day9 {
         List<Integer> values = Arrays.stream(input.split("")).map(Integer::parseInt).toList();
 
         List<String> diskMap = new ArrayList<>();
-        List<Value> valuesAddedMap = new ArrayList<>();
-        List<Dot> dotMap = new ArrayList<>();
+        List<Value> valuesAddedMap = new ArrayList<>(); // value, first index, how many times was added
+        List<Dot> dotMap = new ArrayList<>(); // first index, how many times was added
 
+        // arranging initial disk map
         var dotCount = 0;
         var indexCount = 0;
         for (int i = 0; i < values.size(); i++) {
@@ -27,7 +28,7 @@ public class Day9 {
             for (int j = 0; j < timesToAdd; j++) {
                 indexCount++;
                 if (isEven(i)) {
-                    diskMap.add(String.valueOf(i / 2)); // id
+                    diskMap.add(String.valueOf(i / 2));
                 } else {
                     diskMap.add(".");
                     dotCount++;
@@ -40,23 +41,23 @@ public class Day9 {
         dotMap.forEach(System.out::println);
         valuesAddedMap.forEach(System.out::println);
 
-        // part one
-//        List<String> newMap = new ArrayList<>(diskMap);
-//        int iterations = isEven(dotCount) ? dotCount : dotCount + 1;
-//        for (int i = 0; i < iterations; i++) {
-//            newMap = moveValuesPartOne(newMap);
-//        }
-//
-//        var sum = calculateChecksum(newMap);
-//        System.out.println("Checksum: " + sum);
+        // moving values - part one
+        List<String> newMap = new ArrayList<>(diskMap);
+        // i'm sublisting the dot part, that's why dotCount is used
+        int iterations = isEven(dotCount) ? dotCount : dotCount + 1;
+        for (int i = 0; i < iterations; i++) {
+            newMap = moveValuesPartOne(newMap);
+        }
 
-        // part two
+        var sum = calculateChecksum(newMap);
+        System.out.println("Checksum: " + sum);
+
+        // moving values - part two
         diskMap = doPartTwo(diskMap, valuesAddedMap, dotMap);
         printDiskMap(diskMap);
 
-        var sum = calculateChecksumPartTwo(diskMap);
-        System.out.println("Checksum: " + sum);
-
+        var partTwoSum = calculateChecksumPartTwo(diskMap);
+        System.out.println("Checksum: " + partTwoSum);
     }
 
     private static List<String> doPartTwo(List<String> diskMap,
@@ -64,39 +65,42 @@ public class Day9 {
                                           List<Dot> dotMap) {
 
         for (int j = valuesAdded.size() - 1; j >= 0; j--) {
-
             var valueObj = valuesAdded.get(j);
-            var value = valueObj.getValue(); // ultimo valor colocado
-            var valueSize = valueObj.getSize(); // tamanho dele
-            var valueIndex = valueObj.getIndex(); // primeiro index
+            var value = valueObj.getValue(); // target value - last value put
+            var valueSize = valueObj.getSize(); // how many times target value was put
+            var valueIndex = valueObj.getIndex(); // target value first index
 
             long firstDotThatFits = -1;
             long remainingDots = 0;
             for (int i = 0; i < dotMap.size(); i++) {
                 if (dotMap.get(i).getSize() >= valueSize) {
-                    // se o tamanho do pontinho for = ou > que o tamanho do valor, coloca la
+                    // if dot section size >= valueSize, it fits, so put it there
                     firstDotThatFits = dotMap.get(i).getIndex();
 
+                    // check for remaining dots (if dot section size is bigger than value size)
                     if (dotMap.get(i).getSize() - valueSize > 0) {
                         remainingDots = dotMap.get(i).getSize() - valueSize;
+
+                        // add remaining dots to map (new index = old index + dots that were used by value)
+                        dotMap.add(new Dot(firstDotThatFits + valueSize, remainingDots));
                     }
 
-                    // excluir as dots usadas, ver se sobrou alguma, adicionar e ordenar novamente
+                    // remove replaced dots from map and sort map again, break to stop looking for dots
                     dotMap.remove(i);
-                    dotMap.add(new Dot(firstDotThatFits + valueSize, remainingDots));
                     dotMap.sort(Comparator.comparingLong(Dot::getIndex));
-
                     break;
                 }
             }
 
-            if (firstDotThatFits != -1) { // se for -1 quer dizer q nao achou lugar q cabe
+            // if firstDotThatFits == -1, no suitable dot section found, so dont move value
+            // if dot section index > value index, dots are after value, so dont move value
+            if (firstDotThatFits != -1 && firstDotThatFits < valueIndex) {
                 for (int i = 0; i < valueSize; i++) {
-                    // substituir . pelo valor
+                    // replace . for value
                     diskMap.set((int) firstDotThatFits, value);
                     firstDotThatFits++;
 
-                    // substituir o valor no final por .
+                    // replace value at old place for .
                     diskMap.set((int) valueIndex, ".");
                     valueIndex++;
                 }
@@ -107,10 +111,9 @@ public class Day9 {
     }
 
     private static long calculateChecksumPartTwo(List<String> diskMap) {
-
         long sum = 0;
         for (int i = 0; i < diskMap.size(); i++) {
-            if (diskMap.get(i) != ".") {
+            if (!Objects.equals(diskMap.get(i), ".")) {
                 long value = Long.parseLong(diskMap.get(i));
 
                 long partialSum = value * i;
@@ -119,7 +122,6 @@ public class Day9 {
         }
 
         return sum;
-        // 8509897721111 too high
     }
 
     private static class Dot {
@@ -183,10 +185,9 @@ public class Day9 {
         long sum = 0;
 
         for (int i = 0; i < sublist.size(); i++) {
-            var id = i;
             var value = Long.parseLong(sublist.get(i));
 
-            var partialSum = id * value;
+            var partialSum = i * value;
             sum = sum + partialSum;
         }
 
@@ -207,8 +208,6 @@ public class Day9 {
         var valueToMove = diskMap.get(lastNumberIndex);
         diskMap.set(firstDotIndex, valueToMove);
         diskMap.set(lastNumberIndex, ".");
-
-//        printDiskMap(diskMap);
 
         return diskMap;
     }
