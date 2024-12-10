d4f025ff2493039e597826a9a5aa5f5baac6e453
gabiaabreu <gabiabr3u@gmail.com>
2024-12-10 08:43:17 -0300

    feat: day 9 part two

--- src/Day9.java
+++ src/Day9.java
@@ -8,13 +8,24 @@ public class Day9 {
         String input = Files.readString(Path.of("./src/resources/day9_input.txt"));
 
         List<Integer> values = Arrays.stream(input.split("")).map(Integer::parseInt).toList();
+
         List<String> diskMap = new ArrayList<>();
+        List<Value> valuesAddedMap = new ArrayList<>();
+        List<Dot> dotMap = new ArrayList<>();
 
         var dotCount = 0;
+        var indexCount = 0;
         for (int i = 0; i < values.size(); i++) {
             long timesToAdd = values.get(i);
 
+            if (isEven(i) && timesToAdd > 0) {
+                valuesAddedMap.add(new Value(String.valueOf(i / 2), indexCount, timesToAdd));
+            } else if (!isEven(i) && timesToAdd > 0) {
+                dotMap.add(new Dot(indexCount, timesToAdd));
+            }
+
             for (int j = 0; j < timesToAdd; j++) {
+                indexCount++;
                 if (isEven(i)) {
                     diskMap.add(String.valueOf(i / 2)); // id
                 } else {
@@ -24,19 +35,126 @@ public class Day9 {
             }
         }
 
-//        printDiskMap(diskMap);
+        printDiskMap(diskMap);
 
+        dotMap.forEach(System.out::println);
+        valuesAddedMap.forEach(System.out::println);
+        
         // part one
-        List<String> newMap = new ArrayList<>(diskMap);
-        int iterations = isEven(dotCount) ? dotCount : dotCount + 1;
-        for (int i = 0; i < iterations; i++) {
-            newMap = moveValuesPartOne(newMap);
+//        List<String> newMap = new ArrayList<>(diskMap);
+//        int iterations = isEven(dotCount) ? dotCount : dotCount + 1;
+//        for (int i = 0; i < iterations; i++) {
+//            newMap = moveValuesPartOne(newMap);
+//        }
+//
+//        var sum = calculateChecksum(newMap);
+//        System.out.println("Checksum: " + sum);
+
+        // part two
+        diskMap = doSomething(diskMap, valuesAddedMap, dotMap);
+        printDiskMap(diskMap);
+
+    }
+
+    private static List<String> doSomething(List<String> diskMap,
+                                            List<Value> valuesAdded,
+                                            List<Dot> dotMap) {
+
+        for (int j = valuesAdded.size() - 1; j >= 0; j--) {
+
+            var valueObj = valuesAdded.get(j);
+            var value = valueObj.getValue(); // ultimo valor colocado
+            var valueSize = valueObj.getSize(); // tamanho dele
+            var valueIndex = valueObj.getIndex(); // primeiro index
+
+            long firstDotThatFits = -1;
+            long remainingDots = 0;
+            for (int i = 0; i < dotMap.size(); i++) {
+                if (dotMap.get(i).getSize() >= valueSize) {
+                    // se o tamanho do pontinho for = ou > que o tamanho do valor, coloca la
+                    firstDotThatFits = dotMap.get(i).getIndex();
+
+                    if (dotMap.get(i).getSize() - valueSize > 0) {
+                        remainingDots = dotMap.get(i).getSize() - valueSize;
+                    }
+
+                    // excluir as dots usadas, ver se sobrou alguma, adicionar e ordenar novamente
+                    dotMap.remove(i);
+                    dotMap.add(new Dot(firstDotThatFits + valueSize, remainingDots));
+                    dotMap.sort(Comparator.comparingLong(Dot::getIndex));
+
+                    break;
+                }
+            }
+
+            if (firstDotThatFits != -1) { // se for -1 quer dizer q nao achou lugar q cabe
+                for (int i = 0; i < valueSize; i++) {
+                    // substituir . pelo valor
+                    diskMap.set((int) firstDotThatFits, value);
+                    firstDotThatFits++;
+
+                    // substituir o valor no final por .
+                    diskMap.set((int)valueIndex, ".");
+                    valueIndex++;
+                }
+            }
         }
 
-        printDiskMap(newMap);
+        return diskMap;
+    }
 
-        var sum = calculateChecksum(newMap);
-        System.out.println("Checksum: " + sum);
+    private static class Dot {
+
+        private long index;
+        private long size;
+
+        public long getSize() {
+            return size;
+        }
+
+        public long getIndex() {
+            return index;
+        }
+
+        public Dot(long index, long size) {
+            this.index = index;
+            this.size = size;
+        }
+
+        @Override
+        public String toString() {
+            return "Index: " + index + ", Size: " + size;
+        }
+    }
+
+    private static class Value {
+
+        private String value;
+        private long index;
+        private long size;
+
+        public Value(String value, long index, long size) {
+            this.value = value;
+            this.index = index;
+            this.size = size;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public long getIndex() {
+            return index;
+        }
+
+        public long getSize() {
+            return size;
+        }
+
+        @Override
+        public String toString() {
+            return "Value: " + value + ", Index: " + index + ", Size: " + size;
+        }
     }
 
     private static long calculateChecksum(List<String> diskMap) {
