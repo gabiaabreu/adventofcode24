f758e7a65f161c35aa0fcd41b50d92bdf3ce26cb
gabiaabreu <gabiabr3u@gmail.com>
2024-12-13 10:46:17 -0300

    feat: day 13 part one - input parse

--- src/Day13.java
+++ src/Day13.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class Day13 {
@@ -9,38 +10,23 @@ public class Day13 {
         File file = new File("./src/resources/day13_input.txt");
         BufferedReader reader = new BufferedReader(new FileReader(file));
 
-//        String line;
-//        while ((line = reader.readLine()) != null) {
-//            var intList = Arrays.stream(line.trim().split("\\s+"))
-//                    .map(Integer::valueOf)
-//                    .toList();
-//
-//            reportsList.add(intList);
-//        }
+        var machineRules = getInputRules(reader);
 
+        var totalTokensSpent = 0;
+        for (var machine : machineRules) {
+            MachineResult result = moveClaw(machine.getTargetX(), machine.getTargetY(),
+                    machine.getaX(), machine.getaY(), machine.getbX(), machine.getbY());
 
-        var lala = findOptimalSolution(8400, 5400, 94, 34, 22, 67);
-        var lala2 = findOptimalSolution(12748, 12176, 26, 66, 67, 21);
-        var lala3 = findOptimalSolution(7870, 6450, 17, 86, 84, 37);
-        var lala4 = findOptimalSolution(18641, 10279, 69, 23, 27, 71);
-        var oi = 1;
-
-    }
-
-    private static class MachineResult {
-        int buttonAPresses;
-        int buttonBPresses;
-        int tokensSpent;
-
-        MachineResult(int buttonAPresses, int buttonBPresses, int tokensSpent) {
-            this.buttonAPresses = buttonAPresses;
-            this.buttonBPresses = buttonBPresses;
-            this.tokensSpent = tokensSpent;
+            if(result.getTokensSpent() != -1) {
+                totalTokensSpent += result.getTokensSpent();
+            }
         }
+
+        System.out.println(totalTokensSpent);
     }
 
     // BFS
-    private static MachineResult findOptimalSolution(int targetX, int targetY, int aX, int aY, int bX, int bY) {
+    private static MachineResult moveClaw(int targetX, int targetY, int aX, int aY, int bX, int bY) {
         int maxPresses = 100;
 
         // Fila para explorar os estados
@@ -94,6 +80,78 @@ public class Day13 {
         return new MachineResult(-1, -1, -1);
     }
 
+    // DFS
+    // nao eh bom pra esse caso
+    private static void findOptimalSolution(int buttonAPresses, int buttonBPresses, int currentX, int currentY,
+                                            int targetX, int targetY, int tokensSpent) {
+        // base case
+        if (buttonAPresses >= 100 || buttonBPresses >= 100
+                || currentX > targetX || currentY > targetY) {
+            System.out.println("x: " + currentX + ", y: " + currentY);
+            return;
+        }
+
+        // recursive case
+        if (currentX == targetX && currentY == targetY) {
+            return;
+        }
+
+        findOptimalSolution(buttonAPresses + 1, buttonBPresses,
+                currentX + 94, currentY + 34, targetX, targetY,
+                tokensSpent + 3);
+        findOptimalSolution(buttonAPresses, buttonBPresses + 1,
+                currentX + 34, currentY + 67, targetX, targetY,
+                tokensSpent + 1);
+    }
+
+    private static List<MachineRules> getInputRules(BufferedReader reader) throws IOException {
+        List<MachineRules> machineRules = new ArrayList<>();
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.isBlank()) {
+                break;
+            }
+
+            var rule = new MachineRules();
+            for (int i = 0; i < 3 && line != null; i++) {
+                String ruleName = line.split(":")[0].trim();
+                String ruleValue = line.split(":")[1].trim();
+
+                switch (ruleName) {
+                    case "Button A" -> {
+                        var values = ruleValue.trim().split(",");
+                        var axValue = Integer.parseInt(values[0].replace("X+", ""));
+                        var ayValue = Integer.parseInt(values[1].trim().replace("Y+", ""));
+
+                        rule.setaX(axValue);
+                        rule.setaY(ayValue);
+                    }
+                    case "Button B" -> {
+                        var values = ruleValue.trim().split(",");
+                        var bxValue = Integer.parseInt(values[0].replace("X+", ""));
+                        var byValue = Integer.parseInt(values[1].trim().replace("Y+", ""));
+
+                        rule.setbX(bxValue);
+                        rule.setbY(byValue);
+                    }
+                    case "Prize" -> {
+                        var values = ruleValue.trim().split(",");
+                        var xValue = Integer.parseInt(values[0].replace("X=", ""));
+                        var yValue = Integer.parseInt(values[1].trim().replace("Y=", ""));
+
+                        rule.setTargetX(xValue);
+                        rule.setTargetY(yValue);
+                    }
+                }
+                line = reader.readLine();
+            }
+            machineRules.add(rule);
+        }
+
+        return machineRules;
+    }
+
     private static class State {
         int buttonAPresses;
         int buttonBPresses;
@@ -130,39 +188,81 @@ public class Day13 {
         }
     }
 
-    // DFS
-    // nao eh bom pra esse caso
-    private static void findOptimalSolution(int buttonAPresses, int buttonBPresses, int currentX, int currentY,
-                                            int targetX, int targetY, int tokensSpent) {
-        // base case
-        if (buttonAPresses >= 100 || buttonBPresses >= 100
-                || currentX > targetX || currentY > targetY) {
-            System.out.println("x: " + currentX + ", y: " + currentY);
-            return;
+    private static class MachineResult {
+        int buttonAPresses;
+        int buttonBPresses;
+        int tokensSpent;
+
+        MachineResult(int buttonAPresses, int buttonBPresses, int tokensSpent) {
+            this.buttonAPresses = buttonAPresses;
+            this.buttonBPresses = buttonBPresses;
+            this.tokensSpent = tokensSpent;
         }
 
-        // recursive case
-        if (currentX == targetX && currentY == targetY) {
-            return;
+        public int getTokensSpent() {
+            return tokensSpent;
+        }
+    }
+
+    private static class MachineRules {
+        int aX;
+        int aY;
+        int bX;
+        int bY;
+        int targetX;
+        int targetY;
+
+        public MachineRules() {
+
         }
 
-        findOptimalSolution(buttonAPresses + 1, buttonBPresses,
-                currentX + 94, currentY + 34, targetX, targetY,
-                tokensSpent + 3);
-        findOptimalSolution(buttonAPresses, buttonBPresses + 1,
-                currentX + 34, currentY + 67, targetX, targetY,
-                tokensSpent + 1);
+        public int getaX() {
+            return aX;
+        }
+
+        public int getaY() {
+            return aY;
+        }
+
+        public int getbX() {
+            return bX;
+        }
+
+        public int getbY() {
+            return bY;
+        }
+
+        public int getTargetX() {
+            return targetX;
+        }
+
+        public int getTargetY() {
+            return targetY;
+        }
+
+        public void setaX(int aX) {
+            this.aX = aX;
+        }
+
+        public void setaY(int aY) {
+            this.aY = aY;
+        }
+
+        public void setbX(int bX) {
+            this.bX = bX;
+        }
+
+        public void setbY(int bY) {
+            this.bY = bY;
+        }
+
+        public void setTargetX(int targetX) {
+            this.targetX = targetX;
+        }
+
+        public void setTargetY(int targetY) {
+            this.targetY = targetY;
+        }
     }
 
-    private static int calculateTokens(int buttonA, int buttonB) {
-        return buttonA * 3 + buttonB;
-    }
-
-    private static int calculateXAxis(int buttonA, int buttonB) {
-        return buttonA * 94 + buttonB * 34;
-    }
-
-    private static int calculateYAxis(int buttonA, int buttonB) {
-        return buttonA * 34 + buttonB * 67;
-    }
 }
