db954e3655e05b22e21569a58ae32f3dbb69cd61
gabiaabreu <gabiabr3u@gmail.com>
2024-12-20 17:15:59 -0300

    feat: day 20 part one

--- src/Day20.java
+++ src/Day20.java
@@ -5,10 +5,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Day20 {
 
@@ -30,6 +27,7 @@ public class Day20 {
 
         var startPosition = new State();
         var endPosition = new State();
+        List<Position> walls = new ArrayList<>();
         for (int i = 0; i < racetrack.length; i++) {
             for (int j = 0; j < racetrack[0].length; j++) {
                 if (racetrack[i][j] == 'S') {
@@ -38,6 +36,8 @@ public class Day20 {
                 } else if (racetrack[i][j] == 'E') {
                     endPosition.setX(i);
                     endPosition.setY(j);
+                } else if (racetrack[i][j] == '#') {
+                    walls.add(new Position(i, j));
                 }
             }
         }
@@ -46,13 +46,63 @@ public class Day20 {
         startPosition.setPositions(positions);
         endPosition.setPositions(positions);
 
-        var minTimeWithoutCheat = startRace(racetrack, startPosition, endPosition);
+        var minTimeWithoutCheat = startRaceBFS(racetrack, startPosition, endPosition);
         System.out.println("It takes " + minTimeWithoutCheat + " picoseconds to finish race without cheating");
+
+        // part one - cheat removing 1 wall
+        var cheatCount = 0;
+        for (var wall : walls) {
+            racetrack[wall.getRow()][wall.getCol()] = '.';
+
+            var minTimeCheating = startRaceBFS(racetrack, startPosition, endPosition);
+            if (minTimeWithoutCheat - minTimeCheating >= 100) {
+                cheatCount++;
+            }
+
+            racetrack[wall.getRow()][wall.getCol()] = '#';
+        }
+
+        System.out.println(cheatCount + " cheats could save me at least 100 picoseconds");
     }
 
-    private static int startRace(char[][] racetrack, State initialState, State targetState) {
+    private static int startRaceBFS(char[][] map, State initialState, State targetState) {
+        var startX = initialState.getX();
+        var startY = initialState.getY();
+
+        Queue<State> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[map.length][map[0].length];
+
+        queue.offer(new State(startX, startY, 0, new ArrayList<>()));
+        visited[startX][startY] = true;
+
+        while (!queue.isEmpty()) {
+            State current = queue.poll();
+
+            if (current.getX() == targetState.getX() && current.getY() == targetState.getY()) {
+                return current.getCost();
+            }
+
+            for (int[] dir : DIRECTIONS) {
+                int newX = current.getX() + dir[0];
+                int newY = current.getY() + dir[1];
+                var currentPositions = current.getPositions();
+                currentPositions.add(new Position(newX, newY));
+
+                if (newX >= 0 && newX < map.length && newY >= 0 && newY < map[0].length
+                        && map[newX][newY] != '#' && !visited[newX][newY]) {
+                    visited[newX][newY] = true;
+                    queue.offer(new State(newX, newY, current.getCost() + 1, currentPositions));
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    // I dont need Dijkstra to find the shortest path because I know there's only 1 path
+    private static int startRaceDijkstra(char[][] racetrack, State initialState, State targetState) {
         int[][] trackTime = new int[racetrack.length][racetrack[0].length];
-        for (int[] row : trackTime) Arrays.fill(row, Integer.MAX_VALUE); // assign "infinity" to all nodes
+        for (int[] row : trackTime) Arrays.fill(row, Integer.MAX_VALUE);
 
         trackTime[initialState.getX()][initialState.getY()] = 0;
 
@@ -73,19 +123,15 @@ public class Day20 {
                 int newY = y + direction[1];
 
                 if (newX >= 0 && newY >= 0 && newX < racetrack.length && newY < racetrack[0].length // in bounds
-                        && racetrack[newX][newY] != '#') { // not wall
-                    // adds 1 step to current distance
+                        && racetrack[newX][newY] != '#') {
                     int timeTaken = currentState.getCost() + 1;
 
-                    // if lower than cost currently assigned to node, assign new value
                     if (timeTaken < trackTime[newX][newY]) {
                         trackTime[newX][newY] = timeTaken;
 
-                        // adds to positions visited list
                         List<Position> newPositions = new ArrayList<>(currentState.getPositions());
                         newPositions.add(new Position(newX, newY));
 
-                        // add new state
                         stateQueue.offer(new State(newX, newY, timeTaken, newPositions));
                     }
                 }
